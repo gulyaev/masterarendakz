3464696ec3a6e3ecd0e000e308085c196e86a52d
gulyaev <gulyaev.gvs@gmail.com>
2023-09-30 19:49:27 +0600

    Mapping Messages and Posts

--- client/src/components/Dialogs/Dialogs.jsx
+++ client/src/components/Dialogs/Dialogs.jsx
@@ -3,17 +3,50 @@ import DialogItem from "./DialogItem";
 import Message from "./Message";
 import classes from "../../css/Dialogs.module.css";
 
+
 const Dialogs = () => {
+    let dialogsData = [
+        { id: 1, name: 'John' },
+        { id: 2, name: 'Pall' },
+        { id: 3, name: 'Jeff' },
+        { id: 4, name: 'Mike' },
+        { id: 5, name: 'Alex' },
+        { id: 6, name: 'Den' },
+    ];
+
+    let messagesData = [
+        { id: 1, name: 'John', message: 'Hi' },
+        { id: 2, name: 'Pall', message: 'How are you?' },
+        { id: 3, name: 'Jeff', message: 'Yo!' },
+        { id: 4, name: 'Mike', message: 'Yo!' },
+        { id: 5, name: 'Alex', message: 'Yo!' }
+    ];
+
+    let dialogsElements = dialogsData.map(dialogItem => <DialogItem name={dialogItem.name} id={dialogItem.id} />)
+    let messagesElements = messagesData.map(messagesItem => <Message name={messagesItem.name} message={messagesItem.message} id={messagesItem.id} />)
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogs__users + " " + classes.users}>
                 <DialogItem name="Анна" id={1} />
                 <DialogItem name="Александра" id={2} />
                 <DialogItem name="Вячеслав" id={3} />
+
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+
+                {dialogsElements}
+
             </div>
             <div className={classes.dialogs__messages + " " + classes.messages}>
                 <Message message="Привет" />
                 <Message message="Как дела?" />
+
+                <Message name={messagesData[0].name} message={messagesData[0].message} id={messagesData[0].id} />
+                <Message name={messagesData[1].name} message={messagesData[1].message} id={messagesData[1].id} />
+
+                {messagesElements}
+
             </div>
 
         </div>
--- client/src/components/Profile/MyPosts/MyPosts.jsx
+++ client/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    let postsData = [
+        { id: 1, message: 'Hi! How are you?', likesCount: 18 },
+        { id: 2, message: 'It is my first post', likesCount: 35 },
+        { id: 3, message: 'Where are you from?', likesCount: 35 }
+    ];
+
+    let PostsElements = postsData.map(postItem => <Post message={postItem.message} likesCount={postItem.likesCount} />)
+
     return (
         <div>
-            <Post />
-            <Post />
-            <Post />
+            <Post message="Наша миссия:
+            ускорить каръерный рост мастеров за счет подсвечивания топов индустрии
+            ускорить рост бизнеса за счет подсвечивания топов мастеров
+            повысить эффективность за счет расположения
+            конкурентное преимущество за счет скорости доставки самой передовой информации"/>
+            <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
+            <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
+
+            {PostsElements}
         </div>
     )
 }
