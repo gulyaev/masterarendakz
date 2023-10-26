4c822700a2b0944fc65b971306140f5c0f4d1d75
gulyaev <gulyaev.gvs@gmail.com>
2023-10-26 15:00:48 +0600

    Docker-compose server-postgres

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
+    user: "postgres", //можно назвать как вам нравится
+    password: "password123",//как вам нравится
     host: "db",
     port: 5432,
-    user: "user123", //можно назвать как вам нравится
-    password: "password123",//как вам нравится
-    database: "db123"
+    database: "masterarendakz"
 })
 
 module.exports = pool
--- server/index.js
+++ server/index.js
@@ -3,10 +3,42 @@ const userRouter = require('./routes/user.routes')
 const postRouter = require('./routes/post.routes')
 const authRouter = require("./routes/auth.routes");
 const config = require("config");
+const pool = require("./db")
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('HELLO POSTGRESS + NODEJSdsdfsf')
+app.get('/', async (req, res) => {
+    res.send('HELLO POSTGRESS + NODEJS')
+})
+
+app.post('/', async (req, res) => {
+    const {name, content} = req.body
+    try {
+        await pool.query(`INSERT INTO sales (name, content) VALUES ($1, $2)`, [name, content])
+        res.status(200).send({message: "Successfuly added child"})
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
+app.get('/setup', async (req, res) => {
+    try {
+        await pool.query( "CREATE TABLE sales( id SERIAL PRIMARY KEY, name VARCHAR(100), content VARCHAR(100))")
+        res.status(200).send({message: "Successfuly created table"})
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
+app.get('/create', async (req, res) => {
+    try {
+        await pool.query( "CREATE DATABASE masterarendakz")
+        res.status(200).send({message: "Successfuly created database"})
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 })
 
 app.use(express.json());
